92f469572dad8019a60534bbf60b5fd027760289
umatoma <umatomakun@gmail.com>
2014-09-03 11:14:24 +0900

    メール設定更新

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = {
+    host: 'http://localhost:3000/'
+  }
+  config.action_mailer.default_options = {
+    from: '"ComiGuide" <info@comiguide.net>'
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:            '2126164e93b03168d',
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,6 +66,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {
+    host: 'http://localhost:3000/'
+  }
+  config.action_mailer.default_options = {
+    from: '"ComiGuide" <info@comiguide.net>'
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '21261014954033ad3',
