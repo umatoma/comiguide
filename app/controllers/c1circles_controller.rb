469cc60bd561a2fe80b0e766910f648a2fd93e9f
umatoma <umatomakun@gmail.com>
2014-09-06 10:01:42 +0900

    jsonリクエストの時だけDBにアクセス

--- app/controllers/c1circles_controller.rb
+++ app/controllers/c1circles_controller.rb
@@ -1,10 +1,12 @@
 class C1circlesController < ApplicationController
   def index
     @comic1 = Comic1.find(params[:comic1_id])
-    @c1circles = @comic1.c1circles
-      .includes(c1layout: :c1block)
-      .page(params[:page])
-      .per(15)
+    if request.format.symbol == :json
+      @c1circles = @comic1.c1circles
+        .includes(c1layout: :c1block)
+        .page(params[:page])
+        .per(15)
+    end
 
     respond_to do |format|
       format.html
