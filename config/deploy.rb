cbf4d47c2357256ac2a36599659e3ed266c623d6
umatoma <umatomakun@gmail.com>
2014-12-21 00:33:00 +0900

    be cap enviroment tail:log

--- config/deploy.rb
+++ config/deploy.rb
@@ -132,3 +132,29 @@ namespace :unicorn do
     end
   end
 end
+
+namespace :log do
+  desc 'Tail application log'
+  task :tail do
+    on roles(:app) do
+      tail "#{shared_path}/log/#{fetch(:rails_env)}.log"
+    end
+  end
+
+  def with_verbosity(output_verbosity)
+    old_verbosity = SSHKit.config.output_verbosity
+
+    begin
+      SSHKit.config.output_verbosity = output_verbosity
+      yield
+    ensure
+      SSHKit.config.output_verbosity = old_verbosity
+    end
+  end
+
+  def tail(path)
+    with_verbosity(Logger::DEBUG) do
+      execute "tail -f #{path}"
+    end
+  end
+end
