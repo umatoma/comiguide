d3250cf7f3ed1919120adf51bda292bdba411892
umatoma <umatomakun@gmail.com>
2014-12-24 15:13:06 +0900

    バリデーションエラーを表示する

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -15,6 +15,7 @@ class Api::V1::BaseController < ActionController::Base
 
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from CanCan::AccessDenied, with: :access_denied
 
   protected
@@ -40,6 +41,10 @@ class Api::V1::BaseController < ActionController::Base
   end
 
   def parameter_missing(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
+
+  def record_invalid(exception)
     render json: { error: exception.message }, status: :unprocessable_entity
   end
 
